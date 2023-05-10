88e5b141e4094ddcdb285789a006c8f990bb22e4
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-05-10 12:39:01 -0300

    feat: :sparkles: config project

---
+++ src/Database/territory.js
@@ -0,0 +1,27 @@
+import jalapao from "../Assets/Imgs/Jalapão.jpg"
+import jericoacoara from "../Assets/Imgs/jericoacoara.jpg"
+import natal from "../Assets/Imgs/Natal.jpg"
+import maranhao from "../Assets/Imgs/Maranhão (2).jpg"
+import newYork from "../Assets/Imgs/new york.jpg"
+import london from "../Assets/Imgs/london.jpg"
+import paris from "../Assets/Imgs/Paris.jpg"
+import maldivas from "../Assets/Imgs/maldivas.jpg"
+import sanAndreas from "../Assets/Imgs/San-Andres.jpg"
+
+
+
+export const territory = {
+  brazilian: [
+    {name : "Jericoacoara", img: jericoacoara},
+    {name : "Natal", img: natal},
+    {name :  "Lençóis Maranhenses", img: maranhao},
+    {name :  "Jalapão", img: jalapao}
+  ],
+  international: [ 
+    {name :  "Nova York", img: newYork},
+    {name :  "Paris", img: paris},
+    {name :  "Londres", img: london},
+    {name :  "Maldivas", img: maldivas},
+    {name :  "San andres (Colômbia)", img: sanAndreas}
+]
+}
