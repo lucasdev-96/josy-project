bd6ea435f1bb47c02bbc57831c5839a145227ac8
lucasdev-96 <lucasgodoidev96@gmail.com>
2023-05-23 18:38:27 -0300

    feat: :bug: ajust css

--- src/Components/ServicesComponent.tsx
+++ src/Components/ServicesComponent.tsx
@@ -30,17 +30,17 @@ export const ServicesComponent = () => {
             <h3>Nossos Serviços </h3>
             <div style={{display: 'flex', justifyContent: 'center'}}>
             <section data-aos="fade-right" className='section_infos'>
-                <p><GiPassport />  Vistos</p>
-                <p><FaPassport />  Emissão de passaporte </p>
-                <p><BsTicketPerforatedFill />  Passagens aéreas </p>
-                <p><RiSuitcase3Fill />  Montamos sua bagagem </p>
-                <p><MdChangeCircle />  Mudança de pais</p>
-                <p><BsFillFlagFill />  Nacionalidade</p>
-                <p><FcSimCardChip />  Chip internacional </p>
-                <p><BiHotel />  Hospedagem </p>
-                <p><BsBusFront />  Transfer</p>
-                <p><MdSecurity />  Seguro Viagem </p>
-                <p><Ri24HoursFill />  Assistência durante toda a viagem 24h  </p>
+                <div className="father_infos"><GiPassport className='icons_infos'/>  <p>Vistos</p></div>
+                <div className="father_infos"><FaPassport className='icons_infos' /> <p>Emissão de passaporte </p> </div>
+                <div className="father_infos"><BsTicketPerforatedFill className='icons_infos' /><p>Passagens aéreas </p></div>
+                <div className="father_infos"><RiSuitcase3Fill className='icons_infos' /><p>Montamos sua bagagem </p></div>
+                <div className="father_infos"><MdChangeCircle className='icons_infos' /><p>Mudança de pais </p> </div>
+                <div className="father_infos"><BsFillFlagFill className='icons_infos' /><p>Nacionalidade</p> </div>
+                <div className="father_infos"><FcSimCardChip className='icons_infos' /><p>Chip internacional </p></div>
+                <div className="father_infos"><BiHotel className='icons_infos' /><p>Hospedagem </p></div>
+                <div className="father_infos"><BsBusFront className='icons_infos' /><p>Transfer </p> </div>
+                <div className="father_infos"><MdSecurity className='icons_infos' /><p>Seguro Viagem </p></div>
+                <div className="father_infos"><Ri24HoursFill className='icons_infos' /><p>Assistência durante toda a viagem 24h  </p></div>
             </section>
             </div>
        
