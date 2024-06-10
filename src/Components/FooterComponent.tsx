4aff992c610fb5c2f23e8a5b0c8623425884ddc3
Lucas godoi <lucas.dev@metrify.com.br>
2024-06-10 20:33:34 -0300

    ajusta mensagens

--- src/Components/FooterComponent.tsx
+++ src/Components/FooterComponent.tsx
@@ -1,13 +1,12 @@
-import "../Css/Footer.css"
-import { GrInstagram } from 'react-icons/gr'
-
+import "../Css/Footer.css";
+import { GrInstagram } from 'react-icons/gr';
 
 function FooterComponent() {
   return (
     <footer id="footer" className="footer">
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-6 col-sm-12">
             <h3>Contato</h3>
             <p>Entre em contato conosco para obter mais informações.</p>
             <ul className="list-unstyled">
@@ -15,32 +14,31 @@ function FooterComponent() {
               <li>Email: comercial@jotour.com.br</li>
             </ul>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-6 col-sm-12">
             <h3>Redes Sociais</h3>
             <ul className="list-unstyled">
               <li>
-                <a  target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=5519983201604&text">Whatsapp</a>
+                <a target="_blank" rel="noreferrer" href="https://api.whatsapp.com/send?phone=5519983201604&text">Whatsapp</a>
               </li>
               <li>
                 <a target="_blank" rel="noreferrer" href="https://www.instagram.com/jofpessoa/">Instagram</a>
               </li>
             </ul>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-12 col-sm-12">
             <h3>Parceiros</h3>
             <ul className="list-unstyled">
               <li>
-              <GrInstagram className="links_icon" /> 
-                <a className="links_a" target="_blank" rel="noreferrer" href="https://www.instagram.com/mariaclaraleitao.adv/">Maria Clara Leitão(ADV)</a>
+                <GrInstagram className="links_icon" /> 
+                <a className="links_a" target="_blank" rel="noreferrer" href="https://www.instagram.com/mariaclaraleitao.adv/">Maria Clara Leitão (ADV)</a>
               </li>
-              <li className="li_father">
-                
+              <li>
                 <GrInstagram className="links_icon" /> 
                 <a className="links_a" target="_blank" rel="noreferrer" href="https://www.instagram.com/camentoriadigital/">Ca Mentoria Digital</a>
               </li>
-              <li className="li_father">
+              <li>
                 <GrInstagram className="links_icon" /> 
-                <a className="links_a" target="_blank" rel="noreferrer" href="https://www.instagram.com/lucazgodoy/">Lucas Mazuca Godoi(Dev)</a>
+                <a className="links_a" target="_blank" rel="noreferrer" href="https://www.instagram.com/lucazgodoy/">Lucas Mazuca Godoi (Dev)</a>
               </li>
             </ul>
           </div>
@@ -49,11 +47,9 @@ function FooterComponent() {
       <div className="footer-bottom">
         <div className="container">
           <div className="row">
-            <div className="col-md-12">
-              <p>
-                © {new Date().getFullYear()} Joseane Pessoa - Todos os direitos
-                reservados.
-              </p>
+            <div className="col-md-12 div_infos_footer">
+              <p style={{fontWeight: 200, fontSize: '15px'}}>32.927.004 JOSEANE DE FRANCA PESSOA 32.927.004/0001-06</p>
+              <p style={{fontWeight: 200,  fontSize: '15px'}}>© {new Date().getFullYear()} Joseane Pessoa - Todos os direitos reservados.</p>
             </div>
           </div>
         </div>
--- src/Components/ServicesComponent.tsx
+++ src/Components/ServicesComponent.tsx
@@ -1,49 +1,55 @@
-import '../Css/Values.css'
-import { GiPassport } from 'react-icons/gi'
-import { FaPassport } from 'react-icons/fa'
-import { BsTicketPerforatedFill } from 'react-icons/bs'
-import { RiSuitcase3Fill } from 'react-icons/ri'
-import { MdChangeCircle } from 'react-icons/md'
-import { BsFillFlagFill } from 'react-icons/bs'
-import { FcSimCardChip } from 'react-icons/fc'
-import { BiHotel } from 'react-icons/bi'
-import { BsBusFront } from 'react-icons/bs'
-import { MdSecurity } from 'react-icons/md'
-import { Ri24HoursFill } from 'react-icons/ri'
+import React, { useEffect } from 'react';
+import '../Css/Values.css';
+import { GiPassport } from 'react-icons/gi';
+import { FaPassport } from 'react-icons/fa';
+import { BsTicketPerforatedFill } from 'react-icons/bs';
+import { RiSuitcase3Fill } from 'react-icons/ri';
+import { MdChangeCircle } from 'react-icons/md';
+import { BsFillFlagFill } from 'react-icons/bs';
+import { FcSimCardChip } from 'react-icons/fc';
+import { BiHotel } from 'react-icons/bi';
+import { BsBusFront } from 'react-icons/bs';
+import { MdSecurity } from 'react-icons/md';
+import { Ri24HoursFill } from 'react-icons/ri';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react'
-
-
-
 
 export const ServicesComponent = () => {
     useEffect(() => {
         AOS.init({
-          duration: 1000, // duração da animação em milissegundos
-          easing: 'ease-in-out', // tipo de animação
-          delay: 100, // atraso antes da animação iniciar
+            duration: 1000,
+            easing: 'ease-in-out',
+            delay: 100,
         });
-      }, []);
+    }, []);
+
     return (
-        <div id="services" className="values">
-            <h3>Nossos Serviços </h3>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-            <section data-aos="fade-right" className='section_infos'>
-                <div className="father_infos"><GiPassport className='icons_infos'/>  <p>Vistos</p></div>
-                <div className="father_infos"><FaPassport className='icons_infos' /> <p>Emissão de passaporte </p> </div>
-                <div className="father_infos"><BsTicketPerforatedFill className='icons_infos' /><p>Passagens aéreas </p></div>
-                <div className="father_infos"><RiSuitcase3Fill className='icons_infos' /><p>Montamos sua bagagem </p></div>
-                <div className="father_infos"><MdChangeCircle className='icons_infos' /><p>Imigração Portuguesa</p> </div>
-                <div className="father_infos"><BsFillFlagFill className='icons_infos' /><p>Dupla Cidadania</p> </div>
-                <div className="father_infos"><FcSimCardChip className='icons_infos' /><p>Chip internacional </p></div>
-                <div className="father_infos"><BiHotel className='icons_infos' /><p>Hospedagem </p></div>
-                <div className="father_infos"><BsBusFront className='icons_infos' /><p>Transfer </p> </div>
-                <div className="father_infos"><MdSecurity className='icons_infos' /><p>Seguro Viagem </p></div>
-                <div className="father_infos"><Ri24HoursFill className='icons_infos' /><p>Assistência durante toda a viagem 24h  </p></div>
-            </section>
+        <>
+            <div className="container_service">
+                <p>
+                    Somos uma empresa de Turismo digital, que se destaca no mercado por seu compromisso em auxiliar seus clientes com todo o trajeto da sua viagem.
+                </p>
+                <p>
+                    Com uma equipe de especialistas altamente qualificados para atender antes, durante e no pós, viagem. A nossa empresa oferece soluções personalizadas para atender às necessidades específicas de cada cliente em busca de realizar sonhos.
+                </p>
             </div>
-       
-        </div>
-    )
+
+            <div id="services" className="values">
+                <h3>Nossos Serviços</h3>
+                <div className="section_infos" data-aos="fade-right">
+                    <div className="father_infos"><GiPassport className='icons_infos' />  <p>Vistos</p></div>
+                    <div className="father_infos"><FaPassport className='icons_infos' /> <p>Emissão de passaporte</p> </div>
+                    <div className="father_infos"><BsTicketPerforatedFill className='icons_infos' /><p>Passagens aéreas</p></div>
+                    <div className="father_infos"><RiSuitcase3Fill className='icons_infos' /><p>Montamos sua bagagem</p></div>
+                    <div className="father_infos"><MdChangeCircle className='icons_infos' /><p>Imigração Portuguesa</p></div>
+                    <div className="father_infos"><BsFillFlagFill className='icons_infos' /><p>Dupla Cidadania</p></div>
+                    <div className="father_infos"><FcSimCardChip className='icons_infos' /><p>Chip internacional</p></div>
+                    <div className="father_infos"><BiHotel className='icons_infos' /><p>Hospedagem</p></div>
+                    <div className="father_infos"><BsBusFront className='icons_infos' /><p>Transfer</p></div>
+                    <div className="father_infos"><MdSecurity className='icons_infos' /><p>Seguro Viagem</p></div>
+                    <div className="father_infos"><Ri24HoursFill className='icons_infos' /><p>Assistência durante toda a viagem 24h</p></div>
+                </div>
+            </div>
+        </>
+    );
 }
